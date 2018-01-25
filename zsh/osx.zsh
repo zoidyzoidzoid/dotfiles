88e133c2bb0b4d7461322195b37a18fc0ce5495b
William Stewart <zoidbergwill@gmail.com>
2018-01-25 16:38:42 +0200

    Fix ctrl+x ctrl+e and bring back ... aliases

--- zsh/osx.zsh
+++ zsh/osx.zsh
@@ -2,7 +2,7 @@ if [[ "$(uname -s)" == 'Darwin' ]]; then
   alias vim="mvim -v"
 
   brew_update() {
-    brew update && brew upgrade && brew cleanup -s
+    brew upgrade --cleanup && brew cleanup -s
   }
 fi
 
