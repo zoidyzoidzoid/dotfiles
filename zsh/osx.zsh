71caf500c7de3cb3f1aaba8506d4dd5acf986770
William Stewart <zoidbergwill@gmail.com>
2019-06-19 11:17:21 +0200

    Fix some tmux and homebrew and zsh stuff
    I think my zsh history is still broken though

--- zsh/osx.zsh
+++ zsh/osx.zsh
@@ -2,7 +2,9 @@ if [[ "$(uname -s)" == 'Darwin' ]]; then
   alias vim="nvim"
 
   brew_update() {
-    brew update && brew upgrade && brew cleanup -s
+    brew update
+	brew upgrade
+	brew cleanup -s
   }
   cd() {
     builtin cd $@ && exa
